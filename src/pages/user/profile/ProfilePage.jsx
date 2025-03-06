627f98b0e1f8dde050093b1bb25e437b80e7ec8b
Huỳnh Hoàng Phúc <hoangphuchm11@gmail.com>
2025-03-06 14:26:55 +0700

    fix UI profile

--- src/pages/user/profile/ProfilePage.jsx
+++ src/pages/user/profile/ProfilePage.jsx
@@ -4,6 +4,7 @@ import userApi from "../../../api/api";
 
 const ProfilePage = () => {
   const [user, setUser] = useState(null);
+  const [selectedYear, setSelectedYear] = useState("2024");
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -68,66 +69,62 @@ const ProfilePage = () => {
                 />
 
                 <div className="flex flex-col w-[calc(100%-220px)] max-w-full">
-                  <h2 className="text-xl font-medium leading-none text-black">
+                  <h2 className="text-sm font-medium leading-none text-black">
                     THÔNG TIN LÝ LỊCH KHOA HỌC
                   </h2>
-                  <img
-                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/e1340e03f93cbe05a478b97d870f324a7e2e7240dd6f7d2313088d4a181f448a?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                    className="object-contain self-stretch mt-3 w-full aspect-[200] max-md:max-w-full"
-                    alt="Divider"
-                  />
+                  <hr className="object-contain self-stretch mt-3 w-full max-md:max-w-full" />
 
-                  <div className="mt-6 max-w-full w-[1110px]">
+                  <div className="mt-4 max-w-full w-[1110px]">
                     <div className="flex gap-5 max-md:flex-col">
                       <div className="w-[32%] max-md:ml-0 max-md:w-full">
-                        <div className="flex flex-col grow text-xl font-medium leading-none text-black max-md:mt-10">
+                        <div className="flex flex-col grow text-sm font-medium leading-none text-black max-md:mt-10">
                           <div className="mr-7 max-md:mr-2.5">
                             Mã số sinh viên:{" "}
                             <span className="font-bold">{user?.user_id}</span>
                           </div>
-                          <div className="mt-8">
+                          <div className="mt-4">
                             Họ và tên:{" "}
                             <span className="font-bold">{user?.full_name}</span>
                           </div>
-                          <div className="self-start mt-6">
+                          <div className="self-start mt-4">
                             Giới tính:{" "}
                             <span className="font-bold">
                               {translateGender(user?.gender)}
                             </span>
                           </div>
-                          <div className="mt-7">
+                          <div className="mt-4">
                             Ngày vào trường:{" "}
                             <span className="font-bold">
                               {formatDate(user?.start_date)}
                             </span>
                           </div>
-                          <div className="self-start mt-6">
+                          <div className="self-start mt-4">
                             Khoa:{" "}
                             <span className="font-bold">{user?.faculty}</span>
                           </div>
                         </div>
                       </div>
                       <div className="ml-5 w-[68%] max-md:ml-0 max-md:w-full">
-                        <div className="flex flex-col grow items-start text-xl font-medium leading-none text-black max-md:mt-10 max-md:max-w-full">
+                        <div className="flex flex-col grow items-start text-sm font-medium leading-none text-black max-md:mt-10 max-md:max-w-full">
                           <div>
                             Ngày sinh:{" "}
                             <span className="font-bold">
                               {formatDate(user?.date_of_birth)}
                             </span>
                           </div>
-                          <div className="mt-7">
+                          <div className="mt-4">
                             Số CCCD:{" "}
                             <span className="font-bold">{user?.cccd}</span>
                           </div>
-                          <div className="mt-7">
+                          <div className="mt-4">
                             Email:{" "}
                             <span className="font-bold">{user?.email}</span>
                           </div>
-                          <div className="mt-6">
+                          <div className="mt-4">
                             Số điện thoại:{" "}
                             <span className="font-bold">{user?.phone}</span>
                           </div>
-                          <div className="self-stretch mt-6 max-md:max-w-full">
+                          <div className="self-stretch mt-4 max-md:max-w-full">
                             Địa chỉ liên hệ:{" "}
                             <span className="font-bold">{user?.address}</span>
                           </div>
@@ -138,34 +135,31 @@ const ProfilePage = () => {
                 </div>
               </div>
 
-              <h2 className="self-center mt-8 text-xl font-medium leading-none text-black">
+              <h2 className="self-center mt-4 text-sm font-medium leading-none text-black">
                 TRAO ĐỔI GẦN ĐÂY
               </h2>
-              <img
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/cd7a1c5982e8462259a3e3d76e113a6e43984c17d2b1cd85ca3d67dc1eb29529?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                className="object-contain mt-3.5 ml-3 w-full aspect-[500] max-md:max-w-full"
-                alt="Divider"
-              />
+              <hr className="object-contain self-stretch mt-3 w-full max-md:max-w-full" />
 
               <div className="mt-4 mr-9 max-md:mr-2.5 max-md:max-w-full">
                 <div className="flex gap-5 max-md:flex-col">
                   <div className="w-[28%] max-md:ml-0 max-md:w-full">
                     <div className="flex flex-col pt-7 w-full max-md:mt-10">
-                      <div className="flex z-10 gap-2.5 items-center self-end pr-10 pl-3 mt-7 mr-7 max-w-full text-base font-bold leading-none text-black whitespace-nowrap rounded-md border border-solid bg-zinc-100 border-gray-300 h-[19px] w-[116px] max-md:pr-5 max-md:mr-2.5">
-                        <div className="flex-1 shrink gap-2 self-stretch my-auto basis-0">
-                          2024
-                        </div>
-                        <img
-                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/2dbe78d7da985da06a374ee05556861ce3348cb29278d0bb2f1a0191f4bd13f4?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                          className="object-contain shrink-0 self-stretch my-auto w-2.5 aspect-[2] fill-stone-900"
-                          alt="Dropdown icon"
-                        />
-                      </div>
                       <div className="flex flex-col items-center px-6 pt-6 pb-1.5 mt-0 bg-white rounded-3xl border border-black min-h-[474px] max-md:px-5">
-                        <h3 className="text-lg font-bold leading-none text-black">
-                          Thành tích đóng góp
-                        </h3>
-                        <div className="mt-6 max-w-full min-h-[402px] w-[355px]">
+                        <div className="flex justify-between w-full">
+                          <h3 className="text-sm font-bold leading-none text-black">
+                            Thành tích đóng góp
+                          </h3>
+                          <select
+                            value={selectedYear}
+                            onChange={(e) => setSelectedYear(e.target.value)}
+                            className="flex z-10 gap-2 items-center pr-10 pl-3 text-sm font-bold leading-none text-black whitespace-nowrap rounded-md border border-solid bg-zinc-100 border-gray-300 h-[19px] w-[116px] max-md:pr-5"
+                          >
+                            <option value="2024">2024</option>
+                            <option value="2023">2023</option>
+                            <option value="2022">2022</option>
+                          </select>
+                        </div>
+                        <div className="mt-4 max-w-full min-h-[402px] w-[355px]">
                           <div className="flex flex-col items-center w-full">
                             <img
                               src="https://cdn.builder.io/api/v1/image/assets/TEMP/a85564bf2d993b2f5f5e6338fad61fc05c6ae7fb53a9eb0683310e68382b8e5b?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
@@ -173,7 +167,7 @@ const ProfilePage = () => {
                               alt="Statistics chart"
                             />
                           </div>
-                          <div className="mt-12 w-full text-sm leading-loose text-gray-800 max-w-[355px] min-h-[154px] max-md:mt-10">
+                          <div className="mt-4 w-full text-sm leading-loose text-gray-800 max-w-[355px] min-h-[154px] max-md:mt-10">
                             <div className="flex gap-6 items-start px-8 w-full max-md:px-5">
                               <div className="flex flex-1 shrink gap-2 items-center whitespace-nowrap rounded-lg basis-0">
                                 <div className="flex shrink-0 self-stretch my-auto w-4 h-4 bg-green-500 rounded"></div>
@@ -210,73 +204,39 @@ const ProfilePage = () => {
 
                   <div className="ml-5 w-[72%] max-md:ml-0 max-md:w-full">
                     <div className="mt-2.5 w-full text-xl text-black max-md:mt-10 max-md:max-w-full">
-                      <div className="flex flex-wrap gap-8 mt-7">
-                        <img
-                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/aed835d36a2dd64ee06f5306c36e2c710acd8eb301ba94120a7c5fc6b7e141b0?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                          className="object-contain shrink-0 my-auto aspect-[0.98] rounded-full w-[43px]"
-                          alt="Profile"
-                        />
-                        <div className="relative flex-wrap flex-auto gap-5 px-6 py-3.5 rounded-lg min-h-[67px] shadow-md w-[700px] max-md:px-5 max-md:max-w-full">
-                          <img
-                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/221b7eae7f2cffa306700803709341925ca582e0d8a3fd22387980a74a5c3341?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                            className="object-cover absolute inset-0 size-full"
-                            alt="Background"
-                          />
-                          <div className="relative font-medium leading-5 w-[622px]">
-                            <span className="font-bold">Admin</span>
-                            <br />
-                            Duyệt thông tin bài báo
-                          </div>
-                          <div className="relative my-auto leading-none">
-                            2 tháng
-                          </div>
-                        </div>
-                      </div>
+                      <div className="relative flex flex-col pl-8 border-gray-300">
+                        {Array(5)
+                          .fill(0)
+                          .map((_, index) => (
+                            <div
+                              key={index}
+                              className="relative flex items-start gap-2"
+                            >
+                              {/* Avatar + Đường dọc */}
+                              <div className="relative flex flex-col items-center">
+                                <div className="w-11 h-11 rounded-full bg-white border border-gray-300 flex items-center justify-center">
+                                  <img
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/aed835d36a2dd64ee06f5306c36e2c710acd8eb301ba94120a7c5fc6b7e141b0"
+                                    className="w-10 h-10 rounded-full"
+                                    alt="Profile"
+                                  />
+                                </div>
+                                {index < 4 && (
+                                  <div className="w-[2px] h-12 bg-gray-300"></div>
+                                )}
+                              </div>
 
-                      <div className="flex flex-wrap gap-8 mt-7">
-                        <img
-                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/aed835d36a2dd64ee06f5306c36e2c710acd8eb301ba94120a7c5fc6b7e141b0?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                          className="object-contain shrink-0 my-auto aspect-[0.98] rounded-full w-[43px]"
-                          alt="Profile"
-                        />
-                        <div className="relative flex-wrap flex-auto gap-5 px-6 py-3.5 rounded-lg min-h-[67px] shadow-md w-[700px] max-md:px-5 max-md:max-w-full">
-                          <img
-                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/42fc73bb4a1bb0b00570d85304b9c4474bec9ad763e797096f1dbeb375cb18ef?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                            className="object-cover absolute inset-0 size-full"
-                            alt="Background"
-                          />
-                          <div className="relative font-medium leading-5 w-[622px]">
-                            <span className="font-bold">Admin</span>
-                            <br />
-                            Duyệt thông tin bài báo
-                          </div>
-                          <div className="relative my-auto leading-none">
-                            2 tháng
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className="flex flex-wrap gap-8 mt-7">
-                        <img
-                          src="https://cdn.builder.io/api/v1/image/assets/TEMP/aed835d36a2dd64ee06f5306c36e2c710acd8eb301ba94120a7c5fc6b7e141b0?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                          className="object-contain shrink-0 my-auto aspect-[0.98] rounded-full w-[43px]"
-                          alt="Profile"
-                        />
-                        <div className="relative flex-wrap flex-auto gap-5 px-6 py-3.5 rounded-lg min-h-[67px] shadow-md w-[700px] max-md:px-5 max-md:max-w-full">
-                          <img
-                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5478de77fea7cb18b11e95c7036d93fccba1959e97234cf32e5ab2bf0648abd1?placeholderIfAbsent=true&apiKey=8e7c4b8b7304489d881fbe06845d5e47"
-                            className="object-cover absolute inset-0 size-full"
-                            alt="Background"
-                          />
-                          <div className="relative font-medium leading-5 w-[622px]">
-                            <span className="font-bold">Admin</span>
-                            <br />
-                            Duyệt thông tin bài báo
-                          </div>
-                          <div className="relative my-auto leading-none">
-                            2 tháng
-                          </div>
-                        </div>
+                              {/* Nội dung bên phải */}
+                              <div className="relative flex-auto p-2 bg-white rounded-lg shadow-md w-[700px] max-md:w-full text-sm">
+                                <span className="font-bold">Admin</span>
+                                <br />
+                                Duyệt thông tin bài báo
+                                <div className="absolute top-3 right-3 text-gray-500 text-sm">
+                                  2 tháng
+                                </div>
+                              </div>
+                            </div>
+                          ))}
                       </div>
                     </div>
                   </div>
