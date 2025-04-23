4b36e9703962ee8973fac030f415f0ba49b12103
Apes <21040431.thanh@student.iuh.edu.vn>
2025-04-23 09:26:22 +0700

    fix xog cái cap nhat theo bo loc cua trang artcile admin+phu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,10 +17,9 @@ const Footer = () => {
                 />
               </a>
               <p className="mt-4 text-sm px-2 sm:px-0">
-                Chào mừng các bạn đến với Hệ thống quản lý bài nghiên cứu khoa
-                học của trường Đại học Công nghiệp TP.HCM, hệ thống thông tin
-                cung cấp các bài nghiên cứu của sinh viên và giảng viên đã được
-                kiểm duyệt và đăng lên hệ thống.
+                Chào mừng đến với hệ thống bài nghiên cứu khoa học của Đại học
+                Công nghiệp TP.HCM, nơi lưu trữ các bài nghiên cứu đã kiểm
+                duyệt.
               </p>
             </div>
 
@@ -74,15 +73,16 @@ const Footer = () => {
             {/* Contact Information */}
             <div className="w-full md:w-1/3 px-4 text-center md:text-left">
               <h3 className="font-bold text-red-600 text-sm pb-2">Liên hệ</h3>
-              <p className="text-sm">
-                Trung tâm Quản trị Hệ thống - Trường Đại học Công nghiệp TP.HCM
+              <p className="text-sm pb-1">
+                12 Nguyễn Văn Bảo, Q. Gò Vấp, TP. Hồ Chí Minh Khoa Công nghệ
+                Thông tin - Lầu 1 - Nhà H
+              </p>
+              <p className="text-sm pb-1">
+                <i className="fa fa-phone-square"></i>Phone: 0283.8940 390 - ext
+                838
               </p>
               <p className="text-sm">
-                <i className="fa fa-phone-square mr-1"></i> Phone: 0283.8940 390
-                - ext 838
-              </p>
-              <p className="text-sm">
-                <i className="fa fa-envelope mr-1"></i> E-mail:{" "}
+                <i className="fa fa-envelope"></i>E-mail:{" "}
                 <a
                   href="mailto:csm@iuh.edu.vn"
                   className="text-blue-600 hover:underline"
