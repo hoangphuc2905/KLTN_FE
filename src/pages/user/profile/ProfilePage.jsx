6a9403143988a9f2d053fc2f3aaad17b7eb5b56b
Apes <21040431.thanh@student.iuh.edu.vn>
2025-05-19 14:19:31 +0700

    fix hien thi diem trong thong tin ca nhan

--- src/pages/user/profile/ProfilePage.jsx
+++ src/pages/user/profile/ProfilePage.jsx
@@ -516,7 +516,7 @@ const ProfilePage = () => {
                             {/* Số liệu ở giữa biểu đồ */}
                             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
                               <div className="text-4xl font-bold text-gray-800">
-                                {contributionStats.percentage}%
+                                {contributionStats.percentage.toFixed(1)}%
                               </div>
                               <div className="text-sm text-gray-600 mt-1">
                                 Hoàn thành
